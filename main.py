34201445aaf3622953cf87d7c16d7570ff131872
idkwhatnameE <98589088+idkwhatnameE@users.noreply.github.com>
2022-02-05 19:12:23 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,10 +1,1 @@
-import discord
-from discord.ext import commands
 
-bot = commands.Bot(command_prefix= '!')
-
-@bot.event
-async def on_ready():
-    print("Ready!")
-
-bot.run('OTMwNDA4MTA3ODAzMzA4MDcy.Yd1cEw.8P55CIY1jsxc5c9rzEfYm4BBZ_g')
